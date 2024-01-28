40ce90f5cd36e6170f78bb223f934598c231b05d
John Binze Escol <escoljohnbinze@gmail.com>
2024-01-28 18:24:25 +0800

    sa logs

--- src/pages/admin/logs/index.tsx
+++ src/pages/admin/logs/index.tsx
@@ -15,19 +15,21 @@ const LogsPage = () => {
   const [studentLogs, setStudentLogs] = useState<object[]>([])
   const [staffLogs, setStaffLogs] = useState<object[]>([])
   const [adminLogs, setAdminLogs] = useState<object[]>([])
+  const [saLogs, setSALogs] = useState<object[]>([])
   const [page, setPage] = useState(1)
 
   const fetchRoleLogs = useCallback(async (role: any, setter: (arg0: (prevLogs: any) => any[]) => void) => {
-    const response = await axios.get(`/api/admin/logs/${role}?page=${page}&limit=5`);
-    const newLogs = await response.data;
-    setter(prevLogs => [...prevLogs, ...newLogs]);
-  }, [page]);
+    const response = await axios.get(`/api/admin/logs/${role}?page=${page}&limit=5`)
+    const newLogs = await response.data
+    setter(prevLogs => [...prevLogs, ...newLogs])
+  }, [page])
 
   useEffect(() => {
-    fetchRoleLogs('student', setStudentLogs);
-    fetchRoleLogs('staff', setStaffLogs);
-    fetchRoleLogs('admin', setAdminLogs);
-  }, [fetchRoleLogs]);
+    fetchRoleLogs('student', setStudentLogs)
+    fetchRoleLogs('staff', setStaffLogs)
+    fetchRoleLogs('admin', setAdminLogs)
+    fetchRoleLogs('sa', setSALogs)
+  }, [fetchRoleLogs])
 
   const loadMoreLogs = () => {
     setPage(prevPage => prevPage + 1)
@@ -40,6 +42,9 @@ const LogsPage = () => {
       </Grid>
       <Grid item xs={12} md={12}>
         <ActivityTimeline logs={staffLogs} load={loadMoreLogs} title="Staff Logs"/>
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <ActivityTimeline logs={saLogs} load={loadMoreLogs} title="Student Assistant Logs"/>
       </Grid>
       <Grid item xs={12} md={12}>
         <ActivityTimeline logs={adminLogs} load={loadMoreLogs} title="Admin Logs"/>
