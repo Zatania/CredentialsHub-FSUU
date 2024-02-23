4fc3dd2171bf8440b7882218afa47292325bd91e
John Binze Escol <escoljohnbinze@gmail.com>
2024-02-23 14:46:56 +0800

    Data Privacy Policy changes.

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -960,23 +960,23 @@ const Register = () => {
                                 </Typography>
                               </Box>
                               {/* Insert your data privacy policy content here */}
-                              <strong>Last updated:</strong> February 21, 2024
+                              <strong>Last updated:</strong> February 23, 2024
 
-                              <br/><br/>Father Saturnino Urios University Registrar's Office operates CredentialHub. This page informs you of our policies regarding the collection, use, and disclosure of Personal Information we receive from users of the Site.
+                              <br/><br/>Father Saturnino Urios University Registrar's Office operates CredentialHub. This page informs you of our policies regarding the collection, use, and disclosure of Personal Information we receive from users of the site.
 
                               <br/><br/><strong>Information Collection and Use</strong>
 
-                              <br/><br/>While using our Site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to, your name, email address, postal address, and phone number ("Personal Information").
+                              <br/><br/>While using our site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to, your name, email address, postal address, phone number, and picture holding valid id ("Personal Information").
 
                               <br/><br/><strong>Log Data</strong>
 
-                              <br/><br/> Like many site operators, we collect information that your browser sends whenever you visit our Site ("Log Data"). This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our Site that you visit, the time and date of your visit, the time spent on those pages, and other statistics.
+                              <br/><br/> Like many site operators, we collect information that your browser sends whenever you visit our site ("Log Data"). This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our site that you visit, the time and date of your visit, the time spent on those pages, and other statistics.
 
                               <br/><br/><strong>Cookies</strong>
 
                               <br/><br/>Cookies are files with a small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer's hard drive.
 
-                              <br/><br/>Like many sites, we use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Site.
+                              <br/><br/>Like many sites, we use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our site.
 
                               <br/><br/><strong>Security</strong>
 
@@ -984,7 +984,7 @@ const Register = () => {
 
                               <br/><br/><strong>Changes to This Privacy Policy</strong>
 
-                              <br/><br/>This Privacy Policy is effective as of February 21, 2024 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.
+                              <br/><br/>This Privacy Policy is effective as of February 23, 2024 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.
 
                               <br/><br/>We reserve the right to update or change our Privacy Policy at any time, and you should check this Privacy Policy periodically. Your continued use of the Service after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.
 
